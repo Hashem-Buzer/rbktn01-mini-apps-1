b0bd767439fa2588956e79a5dc4692324f207d86
hashem-buzer <hbuzer98@gmail.com>
2019-12-27 19:54:50 +0100

    some updates still some issues

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,13 +1,24 @@
-// FUNCTIONALITY
-var courtEntities = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+var courtEntities = [
+  0, 0, 0,
+  0, 0, 0,
+  0, 0, 0
+];
 var bool = true;
 var char;
 
+
+// FUNCTIONALITY
+
 // this function responsible for the changes in the array according to the table in html file.
-function entityToggler(id) {
+function entityToggler(id, sign) {
   if (courtEntities[id] === 0) {
-    courtEntities[id] = 1;
-    return true;
+    if (sign === 'X') {
+      courtEntities[id] += 1;
+      return true;
+    } else {
+      courtEntities[id] += 2;
+      return true;
+    }
   }
   return false;
 }
@@ -16,19 +27,21 @@ function entityToggler(id) {
 // VIEW
 
 // this function responsible for the shapes.
-function choose(id) {
+function viewForEntityToggler(id) {
   var entity = document.getElementById(id);
-  if (entityToggler(id)) {
-    if (bool) {
-      char = 'X'
-      entity.innerHTML = (char);
-      bool = false;
-    } else {
-      char = 'O'
-      entity.innerHTML = (char);
-      bool = true;
-    }
+  // if (entityToggler(id, 'X')) {
+  if (bool && entityToggler(id, 'X')) {
+    char = 'X';
+    entity.innerHTML = (char);
+    bool = false;
+  } else if (!bool && entityToggler(id, 'O')) {
+    // bool = false
+    char = 'O';
+    entity.innerHTML = (char);
+    bool = true;
   }
+  // }
+  result(char)
 }
 
 // this function resets the court.
@@ -38,25 +51,23 @@ function reset() {
     entity[i].innerText = '';
     courtEntities[i] = 0;
   }
+  bool = true;
 }
 
+// this function defines either winning or tie
+function result(char) {
+
+  if (((courtEntities[0] && courtEntities[1] && courtEntities[2]) === 1) ||
+    ((courtEntities[3] && courtEntities[4] && courtEntities[5]) === 1) ||
+    ((courtEntities[6] && courtEntities[7] && courtEntities[8]) === 1) ||
 
 
+    ((courtEntities[0] && courtEntities[4] && courtEntities[8]) === 1) ||
+    ((courtEntities[2] && courtEntities[4] && courtEntities[6]) === 1)) {
 
+    alert(`${char} wins`);
 
+  }
+}
 
-
-
-// this function clear the main div.
-// function playButton() {
-//   var mainDiv = document.getElementById("main");
-//   mainDiv.parentNode.innerHTML = ''
-
-//   var courtDiv = document.createElement('div')
-//   courtDiv.style.border = '1px solid black'
-//   courtDiv.style.width = '50px';
-//   courtDiv.style.height = '50px';
-//   courtDiv.style.backgroundColor = 'red';
-//   document.getElementById(mainDiv).appendChild(courtDiv);
-//   // document.body.appendChild(courtDiv);
-// }
+// alert('This is a Tie');
